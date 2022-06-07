5520323ec5a3f89e8fc66211ead71ed52a662576
Tushant Yadav <105536525+tushantydv17@users.noreply.github.com>
2022-06-07 23:33:46 +0530

    Create when_case_program.kt

---
+++ when_case_program.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+  val a = 4
+
+  when (a) {
+    0 -> println("Zero")
+    1 -> println("One")
+    2 -> println("Two")
+    3 -> println("Three")
+    4 -> println("Four")
+    5 -> println("Five")
+    
+    else -> println("no is not in range")
+    
+  }
+  
+
+  
+}
+
